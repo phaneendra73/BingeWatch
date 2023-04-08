2c04cddece154a77b08531e3c0aaa2f3984c2b2d
phaneendra73 <phaneendra3377@gmail.com>
2023-04-08 19:48:51 +0530

    fetch img using api

--- script.js
+++ script.js
@@ -1,12 +1,74 @@
-const apikey="6a18c1c9ec15c5ed95e54eba3af64be4";
-const apiEndPoint="https://api.themoviedb.org/3/"
-const allpath={
-    allmovies :`${apiEndPoint}genre/movie/list?api_key=${apikey}&language=en-US`
+const apikey = "6a18c1c9ec15c5ed95e54eba3af64be4";
+const apiEndPoint = "https://api.themoviedb.org/3/";
+const imgpath= "https://image.tmdb.org/t/p/original";
+const allpath = {
+    getAllCat: `${apiEndPoint}genre/movie/list?api_key=${apikey}&language=en-US`,
+    getmovielist: (id) =>`${apiEndPoint}discover/movie?api_key=${apikey}&with_genres=${id}`
+}
+
+
+
+function loadingcomplete() {
+    getallgeners()
+}
+
+
+function getallgeners() {
+    fetch(allpath.getAllCat)
+        .then(res => res.json())
+        .then(res => {
+            const catogires = res.genres
+            if (Array.isArray(catogires) && catogires.length) {
+                catogires.slice(0,7).forEach(catogire => {
+                    fetchAndbuildSection(allpath.getmovielist(catogire.id),catogire);
+                } );
+            }
+            // console.table(catogires)
+        })
+        .catch(err => console.log(err))
+
+}
+function fetchAndbuildSection(fetchUrl, categoryName){
+    console.log(fetchUrl,categoryName);
+    return fetch(fetchUrl)
+    .then(res => res.json())
+    .then(res => {
+        // console.table(res.results);
+        const movies = res.results;
+        if (Array.isArray(movies) && movies.length) {
+            buildMoviesSection(movies.slice(0,8), categoryName);
+        }
+        return movies;
+    })
+    .catch(err=>console.error(err))
+}
+
+function buildMoviesSection(list, categoryName)
+{
+    console.log(list, categoryName);
+
+    const moviesCont = document.getElementById('movies-cont');
     
+    const moviesListHTML = list.map(item => {
+        return `
+            <img class="movie-item" src="${imgpath}${item.backdrop_path}" alt="${item.title}" />
+        
+    `}).join('');
+
+    const moviesSectionHTML = `
+        <h2 class="movies-sec-head">${categoryName.name} <span class="explore">Explore All ></span></h2>
+        <div class="movies-row">
+            ${moviesListHTML}
+        </div>
+    `
+
+    const div = document.createElement('div');
+    div.className = "movies-section"
+    div.innerHTML = moviesSectionHTML;
+
+    // append html into movies container
+    moviesCont.append(div);
 }
-function loadingcomplete(){
- fetch(allpath.allmovies)
- .then(res => console.log(res))
- .catch(err => console.log(err));
-}
-window.addEventListener('load',loadingcomplete())
+
+
+window.addEventListener('load', loadingcomplete())
